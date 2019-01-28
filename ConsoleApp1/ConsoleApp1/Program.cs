8dbe06fe121501bfc2e3ab4d078d5fd63444e684
aamir <wasi3aamir@gmail.com>
2019-01-28 10:54:43 +0600

    Accepted

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int date = int.Parse(Console.ReadLine());
+            if(date==30|| date == 29)
+            {
+                Console.WriteLine("YES");
+            }else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}
